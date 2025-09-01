c6ad6d6db7060c8a882d256098cc6530dd938a73
edro72018 <edro7201@gmail.com>
2025-08-31 22:02:58 -0600

    Agregación de validaciones de entrada y cálculo de edad

--- age_calculator.py
+++ age_calculator.py
@@ -13,23 +13,44 @@ months = {
 # Obtener nombre del mes actual
 name_month = months[today.month]
 
+maximum_age = 120
+
+
 # Pedimos datos
+#Primer while es para el programa en general
 while True:
     try:
-        user_input_day = int(input("¿Qué día es tu cumpleaños? "))
-        user_input_month = int(input("¿Qué mes cumples años? "))
-        user_input_year = int(input("¿En qué año naciste? "))
-
+        #Es para validar los datos que introduce el usuario
+        while True:
+            user_input_day = int(input("¿Qué día es tu cumpleaños? "))
+            if user_input_day < 1 or user_input_day > 31:
+                print("Por favor, ingresa un día válido.")
+                continue
+            else:
+                break
+        while True:
+            user_input_month = int(input("¿Qué mes cumples años? "))
+            if user_input_month < 1 or user_input_month > 12:
+                print("Por favor, ingresa un mes válido. (1 - 12)")
+                continue
+            else:
+                break
+        while True:
+            user_input_year = int(input("¿En qué año naciste? "))
+            if today.year - user_input_year > maximum_age:
+                print(f"Por favor, ingresa un año de nacimiento válido.")
+                continue
+            else:
+                break
         print(f"Hoy es {today.day} de {name_month}, del año {today.year}")
         print(f"Tu cumpleaños es el {user_input_day} de {months[user_input_month]} de {user_input_year}")
-
         if today.month > user_input_month or (today.month == user_input_month and today.day > user_input_day):
             print(f"Tienes {today.year - user_input_year} años.\n")
         elif today.month == user_input_month and today.day == user_input_day:
             print("¡Feliz cumpleaños!")
         else: 
             print(f"Tienes {today.year - user_input_year - 1} años.\n")
-
+            
         #Salir del programa (s/n)
         quieres_salir = input("¿Quieres salir? (s/n): ")
         if quieres_salir.lower() == "s":
